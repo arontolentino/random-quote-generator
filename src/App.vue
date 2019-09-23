87f8c301dcfcc9b44ebfb1830ef66ec031808bde
Aron Tolentino <arontolentino@outlook.com>
2019-09-22 20:23:58 -0400

    Create initial getRandomQuote method

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <div class="quote">{{ quotes }}</div>
+    <div class="quote">
+      <h1>{{ currentQuote }}</h1>
+      <h2>{{ currentAuthor }}</h2>
+      <button @click="getRandomQuote">Random Quote</button>
+    </div>
   </div>
 </template>
 
@@ -11,15 +15,28 @@ export default {
   name: "app",
   data() {
     return {
-      quotes: null
+      quotesData: null,
+      randomQuote: null,
+      currentQuote: null,
+      currentAuthor: null
     };
+  },
+  methods: {
+    getRandomQuote() {
+      this.randomQuote = this.quotesData.data.quotes[
+        Math.floor(Math.random() * this.quotesData.data.quotes.length)
+      ];
+
+      this.currentQuote = this.randomQuote.quote;
+      this.currentAuthor = this.randomQuote.author;
+    }
   },
   mounted() {
     axios
       .get(
         "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
       )
-      .then(response => (this.quotes = response));
+      .then(response => (this.quotesData = response));
   }
 };
 </script>
